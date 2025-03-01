5d72ba44eacaf99c27647136b813c97fc3bf0b4e
Kiril Yakymchuk <stbestich@gmail.com>
2025-03-01 19:29:17 +0200

    fix: fixed borders

--- src/components/GalleryCard.tsx
+++ src/components/GalleryCard.tsx
@@ -17,7 +17,7 @@ const GalleryCard = ({
 
   return (
     <div
-      className={`card m-2 d-inline-block ${isSelected ? 'border-5' : 'border-0'}`}
+      className={`card m-2 d-inline-block`}
       style={{
         width: isSelected ? '300px' : '200px',
         height: isSelected ? '300px' : '200px',
@@ -26,8 +26,9 @@ const GalleryCard = ({
         transition: 'all 0.3s ease',
         overflow: 'hidden',
         borderRadius: '30px',
-        backgroundColor: 'rgb(102\t103\t108\t)',
-        border: isHovered || isSelected ? 'solid 5px transparent' : 'none',
+        backgroundColor: 'rgb(38, 43, 52)',
+        border: isSelected ? 'solid transparent 7px' : 'none',
+        backgroundClip: 'padding-box',
         boxShadow:
           isHovered || isSelected
             ? '0px 0px 0px 5px rgba(181, 175, 174, 0.4)'
@@ -45,7 +46,7 @@ const GalleryCard = ({
           objectFit: 'cover',
           width: '100%',
           height: '100%',
-          borderRadius: '25px',
+          borderRadius: '20px',
         }}
       />
     </div>
